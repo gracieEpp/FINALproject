9a75f57dccfb44b56bb31c2b225070f5731d2b83
Gracie Eppler <21EppleG@corjesu.org>
2020-07-16 13:54:56 -0700

    with russia pin again

---
+++ FINALproject/menuCountriesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  menuCountriesViewController.swift
+//  FINALproject
+//
+//  Created by Apple on 7/16/20.
+//  Copyright © 2020 Gracie. All rights reserved.
+//
+
+import UIKit
+
+class menuCountriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
