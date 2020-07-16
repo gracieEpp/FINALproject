59f27639afcc8589d9e3d96e9c080caad24e9c04
Gracie Eppler <21EppleG@corjesu.org>
2020-07-15 17:58:17 -0700

    with commented out code

--- FINALproject/searchViewController.swift
+++ FINALproject/searchViewController.swift
@@ -10,22 +10,22 @@ import UIKit
 
 class searchViewController: UIViewController {
     
-    
-    let countryNameArr = ["Italy", "Greece", "Brazil", "Philippines", "Morocco", "Australia", "Ghana"]
-    var searchedCountry = [String]()
-    var searching = false
+//
+//    let countryNameArr = ["Italy", "Greece", "Brazil", "Philippines", "Morocco", "Australia", "Ghana"]
+//    var searchedCountry = [String]()
+//    var searching = false
     
     @IBOutlet weak var tblView: UITableView!
-    @IBOutlet weak var countrySearch: UISearchBar!
+    @IBOutlet weak var searchBar: UISearchBar!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        countrySearch.delegate = self
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        countrySearch.delegate = self
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//    }
   
     @IBAction func bucketTapped(_ sender: UIBarButtonItem) {
     }
@@ -36,44 +36,65 @@ class searchViewController: UIViewController {
     @IBAction func homeTapped(_ sender: UIButton) {
     }
 }
+//
+//extension ViewController: UITableViewDelegate, UITableViewDataSource {
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        if searching {
+//            return searchedCountry.count
+//        } else {
+//            return countryNameArr.count
+//        }
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+//        if searching {
+//            cell?.textLabel?.text = searchedCountry[indexPath.row]
+//        } else {
+//            cell?.textLabel?.text = countryNameArr[indexPath.row]
+//        }
+//        return cell!
+//    }
+//
+//}
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if searching {
-            return searchedCountry.count
-        } else {
-            return countryNameArr.count
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-        if searching {
-            cell?.textLabel?.text = searchedCountry[indexPath.row]
-        } else {
-            cell?.textLabel?.text = countryNameArr[indexPath.row]
-        }
-        return cell!
-    }
-    
-}
 
-extension ViewController: UISearchBarDelegate {
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        searchedCountry = countryNameArr.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
-        searching = true
-        tblView.reloadData()
-    }
-    
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        searching = false
-        searchBar.text = ""
-        tblView.reloadData()
-    }
-    
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//extension ViewController: UISearchBarDelegate {
+//
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        searchedCountry = countryNameArr.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
+//        searching = true
+//        tblView.reloadData()
+//    }
+//
+//    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+//        searching = false
+//        searchBar.text = ""
+//        tblView.reloadData()
+//    }
+//
+//}
+
+
+
+
+
 
 
 //extension ViewController: UITableViewDataSource, UITableViewDelegate {
