65b57730ac024d837417a6fedb4c61f10b31cc17
Gracie Eppler <21EppleG@corjesu.org>
2020-07-17 07:31:16 -0700

    with map annotations figured out

--- FINALproject/mapViewController.swift
+++ FINALproject/mapViewController.swift
@@ -18,42 +18,50 @@ class mapViewController: UIViewController {
     // Do any additional setup after loading the view.
         
         let annotationItaly = MKPointAnnotation()
-                annotationItaly.coordinate = CLLocationCoordinate2DMake(41.8719, 12.5674)
+                annotationItaly.coordinate = CLLocationCoordinate2DMake(41.902782, 12.496366)
+                annotationItaly.title = "Rome, Italy"
                 mapView.addAnnotation(annotationItaly)
                 
                 let annotationGreece = MKPointAnnotation()
-                annotationGreece.coordinate = CLLocationCoordinate2DMake(39.0742, 21.8243)
+                annotationGreece.coordinate = CLLocationCoordinate2DMake( 37.983810, 23.727539)
                 mapView.addAnnotation(annotationGreece)
+                annotationGreece.title = "Athens, Greece"
                 //Greece coordinates: 39.0742° N, 21.8243° E
                 
-                let annotationsBrazil = MKPointAnnotation()
-                annotationsBrazil.coordinate = CLLocationCoordinate2DMake(-15.793889, -47.882778)
-                mapView.addAnnotation(annotationsBrazil)
+                let annotationBrazil = MKPointAnnotation()
+                annotationBrazil.coordinate = CLLocationCoordinate2DMake(-15.793889, -47.882778)
+                annotationBrazil.title = "Brasilia, Brazil"
+                mapView.addAnnotation(annotationBrazil)
                 //Brazil Coordinates: -15.793889, -47.882778
                 
-                let annotationsMorocco = MKPointAnnotation()
-                       annotationsMorocco.coordinate = CLLocationCoordinate2DMake(31.669746, -7.973328)
-                       mapView.addAnnotation(annotationsMorocco)
+                let annotationMorocco = MKPointAnnotation()
+                       annotationMorocco.coordinate = CLLocationCoordinate2DMake(31.669746, -7.973328)
+                        annotationMorocco.title = "Marrakesh, Morocco"
+                       mapView.addAnnotation(annotationMorocco)
                 //Morocco coordinates: 31.669746, -7.973328
                 
-                let annotationsGhana = MKPointAnnotation()
-                              annotationsGhana.coordinate = CLLocationCoordinate2DMake(7.062351, -1.404598)
-                              mapView.addAnnotation(annotationsGhana)
+                let annotationGhana = MKPointAnnotation()
+                              annotationGhana.coordinate = CLLocationCoordinate2DMake(7.062351, -1.404598)
+                                annotationGhana.title = "Mampong, Ghana"
+                              mapView.addAnnotation(annotationGhana)
                 //Ghana coordinates: 7.062351, -1.404598
         //
-                let annotationsPhilippines = MKPointAnnotation()
-                              annotationsPhilippines.coordinate = CLLocationCoordinate2DMake(12.8797, 121.7740)
-                              mapView.addAnnotation(annotationsPhilippines)
+                let annotationPhilippines = MKPointAnnotation()
+                              annotationPhilippines.coordinate = CLLocationCoordinate2DMake(14.599512, 120.984222)
+                                annotationPhilippines.title = "Manila, Philippines"
+                              mapView.addAnnotation(annotationPhilippines)
                 //Philippines coordinates: 12.8797, 121.7740
                 
-                let annotationsAustralia = MKPointAnnotation()
-                                      annotationsAustralia.coordinate = CLLocationCoordinate2DMake(-23.700552, 133.882675)
-                                      mapView.addAnnotation(annotationsAustralia)
+                let annotationAustralia = MKPointAnnotation()
+                                      annotationAustralia.coordinate = CLLocationCoordinate2DMake(-23.700552, 133.882675)
+                                        annotationAustralia.title = "Alice Springs, Australia"
+                                      mapView.addAnnotation(annotationAustralia)
                 //Australia coordinates: -23.700552, 133.882675
                 
-                let annotationsRussia = MKPointAnnotation()
-                                            annotationsRussia.coordinate = CLLocationCoordinate2DMake(61.524010, 105.318756)
-                                            mapView.addAnnotation(annotationsRussia)
+                let annotationRussia = MKPointAnnotation()
+                                            annotationRussia.coordinate = CLLocationCoordinate2DMake(61.524010, 105.318756)
+                                            annotationRussia.title = "Norilsk, Russia"
+                                            mapView.addAnnotation(annotationRussia)
         //Russia Coordinates: 56.833332, 60.583332; 69.3558, 88.1893
                 
             }
